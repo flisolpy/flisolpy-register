cd350fd0532fc87f723a733626cb80bdbcf6407d
Jesus Peralta <gnumux@gmail.com>
2022-04-04 13:45:24 -0400

    [UPDATE] Instalado Admin LTE

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
