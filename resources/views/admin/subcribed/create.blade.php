a01c43880b131d87addda70b5dbe59611959515f
WilliamRoa <williamroa99@gmail.com>
2022-04-20 21:36:48 -0400

    [UPDATE] Actualizado de create de subscribed

--- resources/views/admin/subcribed/create.blade.php
+++ resources/views/admin/subcribed/create.blade.php
@@ -17,59 +17,28 @@
                         {{ Form::open(['url' => '/admin/event', 'method' => 'post', 'files' => true]) }}
                         <div class="row">
                             <div class="col-md-12 form-group">
-                                <label>Nombre del Evento</label>
+                                <label>Nombre y Apellido</label>
                                 <input type="text" name="name" class="form-control required" required>
                             </div>
 
-
                             <div class="col-6 form-group">
-                                <label>Fecha de inicio</label>
-                                <input type="date" name="init_date" class="form-control required" required>
-                            </div>
-
-                            <div class="col-6 form-group">
-                                <label>Fecha de fin</label>
-                                <input type="date" name="finish_date" class="form-control required" required>
-                            </div>
-                            <div class="col-12 form-group">
-                                <label>Descripcion corta del evento</label>
-                                <textarea name="description" id="description" class="form-control required" cols="30" rows="10">
-                                </textarea>
-                            </div>
-
-                            <div class="col-12 form-group">
-                                <label>Contenito</label>
-                                <textarea name="content" id="content" class="form-control required" cols="30" rows="10">
-                                </textarea>
-                            </div>
-                            <div class="col-6 form-group">
-                                <label>Imagen principal</label>
-                                <input type="file" name="image"  class="form-control" required>
+                                <label>Correo Electronico</label>
+                                <input type="text" name="email" class="form-control required" required>
                             </div>
 
                             <div class="col-6 form-group">
-                                <label>Imagen de fondo</label>
-                                <input type="file" name="backgroud_image"  class="form-control" required>
+                                <label>Telefono</label>
+                                <input type="text" name="phone" class="form-control required" required>
                             </div>
 
                             <div class="col-6 form-group">
                                 <label>Habilitar Registro</label>
-                                <select name="enable_register" class="form-control" required>
+                                <select name="confirmed" class="form-control" required>
                                     <option></option>
                                     <option value="1">SI</option>
                                     <option value="0">NO</option>
                                 </select>
                             </div>
-
-                            <div class="col-6 form-group">
-                                <label>Evento Finalizado</label>
-                                <select name="has_finish" required class="form-control">
-                                    <option></option>
-                                    <option value="1">SI</option>
-                                    <option value="0">NO</option>
-                                </select>
-                            </div>
-
 
                             <div class="col-12 text-right">
                                <button type="submit" class="btn btn-primary">Guardar</button>
